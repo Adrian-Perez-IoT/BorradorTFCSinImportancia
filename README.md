1905319a0b7b38c1e76c524d70f595db73cec060
Adrian <perezjadrian@gmail.com>
2019-07-18 02:24:25 -0300

    Se agrego descripcion en el README.ms Se limpio un poco el codigo fuente y se comento con mas detalles los proximos pasos que se deben hacer.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Descripcion del proyecto (¿Incluye dos codigos fuentes, uno para el Backend y el otro para el Cliente )
+/**
+ * @fileoverview    Mi servidor Backend app contiene la LOGICA DEL NEGOCIO. para que el sistema brinde las funciones de alarma que ayuden a proteger el inmueble.  Las funcionalidades principales del backend son 3: 
+
+-1) Dar de alta (GESTIONAR) a los sensores que se usaran (segun el modelo y la ubicacion)
+ 0) Capturar los datos provistos por los sensores.
+ 1) Notificar a los clientes cuando se activa la alarma.
+ 2) Generar informe con el historial de eventos.
+ 3) Configurar el sistema (programar fechas y horas de activacion. Gestionar usuarios.  Gestionar sensores).
+ 
+ 
+/* Sector para la programacion de las funcionalidades fundamentales (en forma de metodos y funciones - no hay acceso direco a la base de datos con sentencias o manejo de rutas especificasde cada entidad) del servidor backend.  
+
+La comunicandose con firebase se realiza mediante el SKD admin y serviceAccouuntKey.json. Y se hace desde el archivo database.js  
--- server.js
+++ server.js
@@ -1,35 +1,20 @@
-/**
- * @fileoverview    Mi servidor.js contiene la LOGICA DEL NEGOCIO en terminos generales para que el sistema de alarma brinde las 3 funcionalidades principales
- * @version         1.0
- * @author          Perez Adrian <perezjadrian@gmail.com>
- * @copyright           
- *
- * History
- * v2.2 – 
- * v2.0 – 
- * v1.1 – Se mejora el acceso a las funciones alojadas en otro archivo .js, para mejor legibilidad de las funciones del servidor backend 
- * v0.0 – Se experimenta el acceso a datos para la lectura del nodo y la lectura/escritura en la nube y Interfaz web
- * ----
- */
+/* requires */
 
-/* Sector para la programacion de las funcionalidades fundamentales (en forma de metodos y funciones - no hay acceso direco a la base de datos con sentencias o manejo de rutas especificasde cada entidad) del servidor backend.  
-La comunicandose con firebase es privilegiada utilizando un archivo .json y se hace desde el archivo baseDeDatos.js  */
-/* Definiremos las funciones y procedimientos fundamentales de la logica de negocio que brindara el servidor backend */
-//hizimos un git pull para recuperar el codigo de la nube y AUTOMATICAMENTE intenta unir con el codigo con el que estoy trabajando localmente...
-
-//importo las funciones de otro programa (database.js)
-
+//requiriendo acceso a la base de datos
 const db = require('./config/database');
 
+//requiriendo acceso al nodo zigbee controlador 
 const coordinador = require('./helpers/comunicNodoZigbee');
 
+//requiriendo express para crear la logica que me permita la entrega de mis fucnionalidades. Tambien se usará para crear en un futuro una capa de seguridad adicional (ejemplo: accesos mediante IP, DSD)
 const express = require('express');
 const app = express();
-app.use(express.json());
+app.use(express.json()); //creo que no lo utilizo.  puedo borrarlo? 
 
 const port = 4000;
 
-var xbee_api = require('xbee-api'); // libreria para facilitar la gestion (leer y escribir tramas) de los nodos zigbee
+//requiriendo la libreria XBEE paraleer y escribir tramas de los nodos zigbee.  Creo que esto debe hacerse un un archivo .js aparte. y desde aqui solo requiero las unciones principales de ese archivo, pero con el nombre de la funcion mas general, como por ejemplo: 
+var xbee_api = require('xbee-api'); // 
 
 const intruso = require('./app/api/models/intrusos');
 var dates = require('./helpers/dates');
@@ -43,25 +28,57 @@ const routesApi = require('./routes/routes-api');
 
 // db.mostrarBD();
 
-var myobj = coordinador.simularTramas();
+var trama = coordinador.simularTramas(); //simular trama debe ser una funcion de una entidad llamada "ModuloXbee".  Luego crear el archivo modulo-xbee.js y tambien un archivo (Entidad) llamado ConexionSerial.js
 console.log("+++++++++++++++++++++++++++     TRAMA SIMULADA     +++++++++++++++++++++++++++++++ \n Objetos:");
-console.log(myobj);
+console.log(trama);
 console.log("+++++++++++++++++++++++++++     ==============     +++++++++++++++++++++++++++++++ \n Objetos:");
 
 
-/** settings */
+/* settings */
 // utilizando mi motor de plantillas: EJS
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 // console.log(__dirname); // PARA SABER EL DIRECTORIO DONDE ESTA MI PROYECTO
 
 
-/** middlewares */
+/* middlewares */
+// Para acceder a las rutas de monitoreoSSS hemos definido middleware para validar al usuario.
+function validateUser(req, res, next) {
+    //esta ruta funcion la utilizaremos para recibir un boolean true en caso de que el usuario se haya logueado correctamente y asi poder: enviarle notificaciones.
+    //llamamos a un metodo que fue creado en el archivo authentication.js
+    //si devolvio error, entonces mostramos el mensaje con los detalles del error(contraseña erronea, usuario inexistente, etc) 
+    //si no existen errores entonces, ¿añadimos al req.body.userId un ID de usuario decodificado?  Y por ultimo devolvemos next() 
 
+}
 
-/** rutas */
+/* rutas */
 app.use(routes);
-app.use('/api', routesApi);
+app.use('/api', routesApi); // esto es solo un ejemplo.  Partiendo de aqui podemos rutear las APLICACIONES basicas de mi servidor (API), esto es: /USUARIO, /POST, /CONFIGURACION, etc 
+
+
+/** Desglosaremos la siguiente ruta de funcionalidad (que se creo a modo de prueba) en componentes mas pequeños, que tengan funciones entendibles y manejables. 
+ *  Las entidades y funciones descubiertas en el desglose son:
+ *      Entidad: MONITOR. 
+ *      Propiedades: intervalo de tiempo de funcionamiento, ¿sensores a funcionar asociados a la propiedad anterior de intervalo de tiempo?.  Trama? 
+ *      Metodos: boolean iniciarMonitoreoPermanente(), boolean iniciarMonitoreoEnUnIntervaloDeTiempo(intervalo),  detenerMonitoreo()
+ *      
+ *      Entidad: EVENTO
+ *      Pripiedades: fecha, hora, lugar, descripcion
+ *      Metodos: 
+ * 
+ *      Entidad: Notificacion
+ *      Pripiedades: fecha, hora, destinatario, titulo, detalles
+ *      Metodos:
+ * 
+ *      Entidad: Usuario
+ *      Pripiedades: nombre, dispositivoMovil, correoElectronico, numeroCelular, etc.
+ *      Metodos:
+ * 
+ *      Entidad: DatabaseRealtime
+ *      Pripiedades: 
+ *      Metodos: Create(), Read(), Update(), Delete()
+ * */
+
 
 
 // Cuando el servidor backend reciba una peticion con la ruta a "http:localhost:4000/start" se llamará a una funcion que 
@@ -70,7 +87,7 @@ app.use('/api', routesApi);
 app.get('/monitorear', (req, res) => {
     var mytrama = coordinador.simularTramas(); // luego reemplazar esta funcion por la funcion que me obtiene tramas REALES constantemente desde mi puerto USB de la notebook
     var evento = coordinador.procesarDatos(mytrama); // envio la trama a una funcion para que procese si se debe o no debe activar la alarma 
-    // si se detecto un evento de peligro entonces envio una notificacion al cliente
+    // si se detecto un evento de peligro entonces envio una notificacion al cliente.
     if (evento == "movimientoPIR" || evento == "openMAGNETICO" || evento == "dangerGAS") {
         console.log("se detecto una amenaza del tipo: ", evento.toString());
         //creo un nuevo registro en mi realtime database
@@ -97,7 +114,7 @@ app.listen(port, (req, res) => {
 
 });
 
-
+//esta ruta esta de mas? no se si podré borrarla sin que me ocaciones problemas (luego averiguarlo)
 app.get('/favicon.ico', function(req, res) {
     res.sendStatus(204);
 });
@@ -117,13 +134,3 @@ app.use(function(err, req, res, next) {
     else
         res.status(500).json({ message: "ERROR 500: Error interno en el servidor!!" });
 });
-
-
-// Para acceder a las rutas de monitoreoSSS hemos definido middleware para validar al usuario.
-function validateUser(req, res, next) {
-    //esta ruta funcion la utilizaremos para recibir un boolean true en caso de que el usuario se haya logueado correctamente y asi poder: enviarle notificaciones.
-    //llamamos a un metodo que fue creado en el archivo authentication.js
-    //si devolvio error, entonces mostramos el mensaje con el estado y datos de dicho error
-    //sino, ¿añadimos al req.body.userId un ID de usuario decodificado?  Y por ultimo devolvemos next() 
-
-}
